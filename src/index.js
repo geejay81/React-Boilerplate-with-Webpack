0dd39b47ce7367300380d307e2bacbdc90205e56
Greg Parr <greg.parr@blueyonder.co.uk>
2018-07-09 21:43:57 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./index.css"
+
+const Index = () => {
+  return <div>React Boilerplate with Webpack</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
